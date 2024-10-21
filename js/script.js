d55fa5638b6732be618dbb86cd034971c822f103
Hendell Miguel <hendelllmiguel@gmail.com>
2024-10-21 12:46:52 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,1 +1,13 @@
+const toggleBtn = document.querySelector('.menu-btn')
+const toggleBtnIcon = document.querySelector('.menu-btn i')
+const dropDownMenu = document.querySelector('.menu-baixo')
+
+toggleBtn.onclick = function () {
+    dropDownMenu.classList.toggle('aberto')
+    const isOpen = dropDownMenu.classList.contains('aberto')
+
+    toggleBtnIcon.classList = isOpen
+    ? 'fa-solid fa-xmark'
+    : 'fa-solid fa-bars'
+}
 
