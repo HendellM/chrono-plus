d26d0f2b22606112b135c75de6c590a2ca22d336
Hendell Miguel <hendelllmiguel@gmail.com>
2024-10-21 12:48:13 -0300

    Update script.js

--- js/script.js
+++ js/script.js
@@ -3,6 +3,7 @@ const toggleBtnIcon = document.querySelector('.menu-btn i')
 const dropDownMenu = document.querySelector('.menu-baixo')
 
 toggleBtn.onclick = function () {
+    console.log("teste")
     dropDownMenu.classList.toggle('aberto')
     const isOpen = dropDownMenu.classList.contains('aberto')
 
